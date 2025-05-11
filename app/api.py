f3f3b8aed05e653c3da091a394d541a3e2ad5ba6
Jashanpreet Singh <137514293+Jashanpreetbrar@users.noreply.github.com>
2025-05-11 17:03:42 +0530

    Update api.py

--- app/api.py
+++ app/api.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
 from app.model import predict_npk
 
 app = FastAPI()
@@ -8,8 +8,12 @@ def root():
     return {"message": "NPK Fertilizer Prediction API"}
 
 @app.get("/predict")
-def get_prediction(steps: int = 5):
-    print(f"[INFO] Predicting next {steps} NPK values...")
+def get_prediction(steps: int = Query(5, gt=0, le=36)):
+    """
+    Predict the next `steps` values for N, P, and K.
+    - steps: number of months to forecast (1–36)
+    """
+    print(f"[INFO] Predicting next {steps} months expected values NPK values...")
     prediction = predict_npk(steps)
-    print(f"[INFO] Prediction complete: {prediction}")
-    return {"NPK_Prediction": prediction}
+    print(f"[INFO] Prediction complete.")
+    return prediction
