c15d90d98f07a52fa684754ca7e5167096730a5a
Jashanpreet Singh <137514293+Jashanpreetbrar@users.noreply.github.com>
2025-05-11 14:19:15 +0530

    Update db.py

--- app/db.py
+++ app/db.py
@@ -1,10 +1,11 @@
 from pymongo import MongoClient
-import ssl
 import os
-uri = os.environ.get("MONGO_URI")
-client = MongoClient(uri, ssl=True, ssl_cert_reqs=ssl.CERT_REQUIRED)
-db = client['fertilizer']
-collection = db['predictions']
+
+MONGO_URI = os.environ.get("MONGO_URI")
+
+client = MongoClient(MONGO_URI)
+db = client["fertilizer"]
+collection = db["predictions"]
 
 def store_prediction(data):
     collection.insert_one({"prediction": data})
