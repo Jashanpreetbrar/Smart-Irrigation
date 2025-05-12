dfc4a026869b5d3fd5634ba58d7d207c57205d6d
Jashanpreet Singh <137514293+Jashanpreetbrar@users.noreply.github.com>
2025-05-12 12:31:27 +0530

    Update api.py

--- app/api.py
+++ app/api.py
@@ -20,7 +20,7 @@ def serve_ui():
     html_path = Path("app/index.html")
     if not html_path.exists():
         return HTMLResponse("<h2>UI file not found</h2>", status_code=404)
-    return html_path.read_text()
+    return html_path.read_text(encoding="utf-8")
 
 @app.get("/predict")
 def get_prediction(steps: int = Query(5, gt=0, le=36)):
